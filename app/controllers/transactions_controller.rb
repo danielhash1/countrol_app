f4afa4340c5a1a7678fb52da3efbf134fd8b24ba
Pawel Nowak <paqlnis81@gmail.com>
2024-04-17 14:40:33 +0200

    creating-wallet-transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,58 +1,60 @@
 class TransactionsController < ApplicationController
-  
-def index
-  @wallet = Wallet.find(params[:wallet_id])
-  @transactions = Transaction.where(wallet: @wallet)
-end
 
-def show
-  @transaction = Transaction.find(params[:id])
-  @wallet = @transaction.wallet
-end
-
-def new
-  @wallet = Wallet.find(params[:wallet_id])
-  @transaction = Transaction.new
-end
-
-def create
-  @transaction = Transaction.new(transaction_params)
-  @transaction.category_id = params[:transaction][:category_id]
-
-  @wallet = Wallet.find(params[:wallet_id])
-  @transaction.wallet = @wallet
-
-  # @wallet = @goal.wallets.where(user: current_user).first
-  # @goal = @wallet.goals.where(user: current_user).first
-  # @transaction.goal = @goal
-
-  if @transaction.save
-    redirect_to transaction_path(@transaction)
-  else
-    render :new, status: :unprocessable_entity
+  def index
+    @wallet = Wallet.find(params[:wallet_id])
+    @transactions = Transaction.where(wallet: @wallet)
   end
-end
 
+  def show
+    @transaction = Transaction.find(params[:id])
+    @wallet = @transaction.wallet
+  end
 
+  def new
+    @wallet = Wallet.find(params[:wallet_id])
+    @transaction = @wallet.transactions.build
+    @transaction.transaction_type = params[:transaction_type]
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.category_id = params[:transaction][:category_id]
+    @transaction.goal_id = params[:transaction][:goal_id]
+    @wallet = Wallet.find(params[:wallet_id])
+    @transaction.wallet = @wallet
+
+    # @wallet = @goal.wallets.where(user: current_user).first
+    # @goal = @wallet.goals.where(user: current_user).first
+    # @transaction.goal = @goal
+
+    if @transaction.save
+      redirect_to transaction_path(@transaction)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def edit
     @transaction = Transaction.find(params[:id])
   end
+
   def update
     @transaction = Transaction.find(params[:id])
     @transaction.update(transaction_params)
     redirect_to transaction_path(@transaction)
   end
+
   def destroy
     @transaction = Transaction.find(params[:id])
     @wallet = @transaction.wallet
     @transaction.destroy
     redirect_to wallet_transactions_path(@wallet)
   end
-private
+
+  private
 
   def transaction_params
-    params.require(:transaction).permit(:title, :description, :date, :amount, :category_id)
+    params.require(:transaction).permit(:title, :description, :date, :amount, :category_id, :transaction_type, :goal_id)
   end
-
 end
