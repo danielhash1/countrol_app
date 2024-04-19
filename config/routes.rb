e850ef84fa91963f0369599284115fa6e22c0d8a
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <hashdan542@gmail.com>
2024-04-19 15:59:11 +0200

    Resolved conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     resources :transactions, only: [:new, :create]
   end
   resources :wallets, only: [ :show, :new, :create, :edit, :update, :destroy] do
-    resources :transactions, only: [:new, :create, :index, :show]
+    resources :transactions, only: [:new, :create, :index, :show, :edit, :uodate, :destroy]
     resources :goals
     resources :categories, only: [:new, :create, :index, :show, :edit, :update]
   end
