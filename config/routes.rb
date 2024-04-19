f6005b7737190ee9cb3e3898b5f3be9ca62db22c
Daniel Hash <108074788+danielhash1@users.noreply.github.com>
2024-04-19 17:12:43 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "wallets#index"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     root 'devise/sessions#new'
