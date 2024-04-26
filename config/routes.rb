8f564d8695962b4bff02ed110075b95d40b2bb49
Jiregna Terfa <jirenlife@gmail.com>
2024-04-26 15:58:39 +0200

    home page fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # devise_scope :user do
   #   root 'devise/sessions#new'
